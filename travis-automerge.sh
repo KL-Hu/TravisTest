6cbb957fdc07c1d077f8bde9e97b5d8dbfa2264c
Avery.Hu <avery.hu@acer.com>
2017-06-19 14:08:56 +0800

    Merge test bug fix
    change push url

--- travis-automerge.sh
+++ travis-automerge.sh
@@ -20,7 +20,7 @@ echo "current branch is '$CURRENT_BRANCH'"
 # Create the URL to push merge to 
 URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
 echo "Repo url is $URL"
-PUSH_URL="https://$GIT_USER:$GIT_PASS@${URL:6} HEAD:refs/heads/master"
+PUSH_URL="https://$GIT_USER:$GIT_PASS@${URL:8} HEAD:refs/heads/master"
 
 if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
     # Checkout the dev branch

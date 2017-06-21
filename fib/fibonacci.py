f6104bea3953bf4206fb5ede4ef083875dd3a9e4
Avery.Hu <avery.hu@acer.com>
2017-06-21 13:16:44 +0800

    Commit test
    commit test

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
 '''This is fib function'''
-def fibonacci(max):
+def fibonacci(maxx):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < max:
+    while first < maxx:
         yield first
         first, second = second, first + second

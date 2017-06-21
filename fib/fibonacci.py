c877a18bc8fb6d8e28ca58d95cfb55d7d65662a0
Avery.Hu <avery.hu@acer.com>
2017-06-21 13:20:23 +0800

    Error test
    test error

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
 '''This is fib function'''
-def fibonacci(maxx):
+def fibonacci(max):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < maxx:
+    while first < max:
         yield first
         first, second = second, first + second

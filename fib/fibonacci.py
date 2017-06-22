722196871867a08e057b2b4e4748098085fd7181
Avery.Hu <avery.hu@acer.com>
2017-06-22 17:11:08 +0800

    Hook test
    test hook

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
 '''This is fib function'''
-def fibonacci(webhook):
+def fibonacci(webhook_1):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < webhook:
+    while first < webhook_1:
         yield first
         first, second = second, first + second

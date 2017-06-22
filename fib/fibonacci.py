1725bc308b2eec516e90536cb82d89efaddfcf80
Avery.Hu <avery.hu@acer.com>
2017-06-22 16:57:39 +0800

    Jenkins hook
    test hook

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
 '''This is fib function'''
-def fibonacci(avery):
+def fibonacci(webhook):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < avery:
+    while first < webhook:
         yield first
         first, second = second, first + second

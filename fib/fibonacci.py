74c497a0997cb4438be543ef68352f7018d28d9a
Avery.Hu <avery.hu@acer.com>
2017-06-22 17:23:01 +0800

    pylint hook test
    test hook

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
 '''This is fib function'''
-def fibonacci(webhook_1):
+def fibonacci(webhook):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < webhook_1:
+    while first < webhook:
         yield first
         first, second = second, first + second

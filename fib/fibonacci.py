cc0fe5e0f950c1278fb083cba1d9f9c54506a7ae
Avery.Hu <avery.hu@acer.com>
2017-06-23 14:07:32 +0800

    test jen p

--- fib/fibonacci.py
+++ fib/fibonacci.py
@@ -1,9 +1,9 @@
 #!/bin/python
 # fibonacci.py
-'''This is fib function o'''
-def fibonacci(webhook):
+'''This is fib function p'''
+def fibonacci(max):
     ''' calculate fibonacci series '''
     first, second = 0, 1
-    while first < webhook:
+    while first < max:
         yield first
         first, second = second, first + second

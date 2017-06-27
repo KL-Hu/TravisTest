46b7924ca4ff30acf90efcd0bae5a16a71250885
Avery.Hu <avery.hu@acer.com>
2017-06-27 18:07:21 +0800

    mail test 2

--- test/test_fib.py
+++ test/test_fib.py
@@ -5,7 +5,7 @@ from nose.tools import assert_equal
 from fib.fibonacci import fibonacci
 
 def test_fib():
-    ''' Test function for fib() pull request'''
+    ''' Test function for fib() pull request tt'''
     myfib = fibonacci(100)
     series = []
     test_case = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
